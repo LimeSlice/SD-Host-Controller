7c8f691df57d7c1642702e156327d16b08f0e08a
LimeSlice <wolffkris7@gmail.com>
2020-10-16 12:30:56 -0600

    Updated uart and starting testing

--- uart_rx.v
+++ uart_rx.v
@@ -10,19 +10,30 @@ module uart_rx(clk, resetn, pin, data, ctrl, state);
 
     assign state = {ctrl[7:3], state_rx_contains_data, ctrl[1:0]};
 
+    always @(data)
+        if (data != 4'b0 && index == 4'b1000) 
+            state_rx_contains_data = 1'b1;
+
     always @(posedge clk, posedge resetn) begin
         if (resetn) begin
-            state_rx_contains_data <= 1'b0;
             index <= 4'b1000;
             data <= 8'b0;
         end
         else begin
-            // Processing stop bit
+            // Processing start bit
             if (index == 4'b1000 && pin == 1'b0) begin
-                state_rx_contains_data <= 1'b0;
                 index <= 4'b0;
                 data <= 8'b0;
             end
-            else if
+            // Fill data register until all data is filled
+            else if (index < 4'b1000) begin
+                index <= index + 1'b1;
+                data[index] <= pin;
+            end
+            // Process stop bit and any bits afterwards
+            else begin
+                index <= index;
+                data <= data;
+            end
         end
     end
